78b0abab918ae882f9c24cc2192176f43529d2ad
Hunter <Hunter@raglandonline.com>
2023-08-02 22:31:26 +0900

    Created Seperate Voice Library script to hold Voice Presets (Ignoring pyCache)

---
+++ azure_speech_VoiceLibrary.py
@@ -0,0 +1,21 @@
+#Library for Voice Profile Presets
+
+#Template for creating Profiles
+{
+# # The language of the voice that speaks.
+# voiceSetting = 'en-US-AshleyNeural'
+# #pitch for the voice
+# pitchSetting = '+1.25st'
+}
+
+voiceLibrary = ["NEURO", "ARIA"]
+
+class Neuro():
+    voiceSetting = 'en-US-AshleyNeural'
+    pitchSetting = '+1.25st'
+
+class Aria():
+    voiceSetting = 'en-US-AriaNeural'
+    pitchSetting = '+0st'
+
+    
